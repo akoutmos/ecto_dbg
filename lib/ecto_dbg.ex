691cd3f6f0711b1228207e3b89dd4371675d71f6
Alex Koutmos <akoutmos@gmail.com>
2023-01-24 22:29:26 -0500

    Initial commit

---
+++ lib/ecto_dbg.ex
@@ -0,0 +1,18 @@
+defmodule EctoDbg do
+  @moduledoc """
+  Documentation for `EctoDbg`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoDbg.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_dbg_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoDbgTest do
+  use ExUnit.Case
+  doctest EctoDbg
+
+  test "greets the world" do
+    assert EctoDbg.hello() == :world
+  end
+end
