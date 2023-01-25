d8c0fbd7c65a9828fb18786bfa57b3f667ffe354
Alex Koutmos <akoutmos@gmail.com>
2023-01-25 15:41:06 -0500

    Updated deps

--- mix.exs
+++ mix.exs
@@ -21,9 +21,17 @@ defmodule EctoDbg.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      # Required dependencies
       {:ecto_sql, "~> 3.7"},
       {:ecto_dev_logger, "~> 0.8.0"},
-      {:erlexec, "~> 2.0"}
+      {:erlexec, "~> 2.0"},
+
+      # Development dependencies
+      {:ex_doc, "~> 0.29.1", only: :dev},
+      {:excoveralls, "~> 0.15.3", only: :test, runtime: false},
+      {:credo, "~> 1.6.1", only: :dev},
+      {:dialyxir, "~> 1.2.0", only: :dev, runtime: false},
+      {:git_hooks, "~> 0.7.3", only: [:test, :dev], runtime: false}
     ]
   end
 end
